a7c77b6e7c2ddd5d7f933b6088fcf87b96ff786c
ultimated19 <edwardhull34@yahoo.com>
2015-06-07 17:53:03 +0100

    Names
    Added a function to read names from a text file, "names.txt".
    Boy names on the left, girl names on the right.

--- generator.py
+++ generator.py
@@ -7,14 +7,33 @@ Created on Thu Apr 09 18:21:33 2015
 
 import random
 
+def readNames(filename):
+    f = open(filename, "r")
+    global lines
+    global columns
+    lines = f.readlines()
+    global boyNames
+    boyNames = []
+    global girlNames
+    girlNames = []
+    for line in lines:
+        columns = line.split()
+        boyNames.append(columns[0])
+        girlNames.append(columns[1])
+    f.close()
+
 def chargen(verbose=0): #The function that will roll a character and define their name
     if(verbose==1):
         print "Generating a new character..."
+        print "Opening default name file..."
+    readNames("names.txt")
     gender = random.choice(['male','female'])
     if(gender=='male'):
-        name = random.choice(['Alfred', 'Ben', 'Collin', 'Dave'])
+        #name = random.choice(['Alfred', 'Ben', 'Collin', 'Dave'])
+        name = random.choice(boyNames)
     else:
-        name = random.choice(['Alice','Becky','Chloe','Dianna'])
+        #name = random.choice(['Alice','Becky','Chloe','Dianna'])
+        name = random.choice(girlNames)
     Rolls = []
     for y in range(0,7):
         Rolls.append(random.randint(1,6)+random.randint(1,6)+random.randint(1,6))
@@ -48,5 +67,7 @@ def chargen(verbose=0): #The function that will roll a character and define thei
     NewVillager["Charisma"] = Rolls[5]
     NewVillager["Comeliness"] = Rolls[6]
     villagers.append(NewVillager)
+    return villagers
     
 global villagers
+villagers = []
